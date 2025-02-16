a5355421d0e3273f45abd7cddaa986dec3e0136a
Artyom <delangame666@gmail.com>
2025-02-16 14:34:05 +0300

    add allowedHosts in vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,15 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-    plugins: [
-        react(),
-        tailwindcss(),
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  server: {
+    allowedHosts: [
+      '4736-185-77-216-5.ngrok-free.app', // Разрешаем этот хост
     ],
-})
+  },
+});
