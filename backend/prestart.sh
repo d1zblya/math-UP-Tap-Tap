025e0d1965e24409f2ac4e970c8f5ea8ac07419c
Artyom <delangame666@gmail.com>
2025-03-01 17:35:57 +0300

    edit prestart.sh

--- backend/prestart.sh
+++ backend/prestart.sh
@@ -8,4 +8,5 @@ echo "Applying database migrations..."
 alembic upgrade head
 
 echo "Starting the application..."
-exec uvicorn src.main:app --host 0.0.0.0 --port 8000 --reload
+#exec uvicorn src.main:app --host 0.0.0.0 --port 8000 --reload
+python ./src/main.py
