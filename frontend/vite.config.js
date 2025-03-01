ac9438c2c76c226d9e0dcb2a55dbb84a922e3f6a
ulver <ivansergshum@gmail.com>
2025-03-01 10:48:29 +0300

    Add https

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     ],
     server: {
         allowedHosts: [
-            '1b593e058ff97756232bb73f00155c38.serveo.net',
+            'mathup.ru',
         ],
     },
 });
