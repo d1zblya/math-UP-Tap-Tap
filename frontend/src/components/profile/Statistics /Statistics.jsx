071b09a09b2f84a99eebfb4cfbfbeaf39804578c
ulver <ivansergshum@gmail.com>
2025-03-14 21:38:41 +0300

    refactoring

--- frontend/src/components/profile/Statistics /Statistics.jsx
+++ frontend/src/components/profile/Statistics /Statistics.jsx
@@ -1,12 +1,20 @@
 import "./Statistics.css"
 import StatsRow from "./StatsRow/StatsRow.jsx";
+import React from "react";
+
+const STATS = [
+    {title: "Часов в игре", value: 52},
+    {title: "Решено примеров", value: 10},
+    {title: "Решено уравнений", value: 3},
+]
 
 const Statistics = () => {
+    let _this = this;
     return (
         <div className="Statistics">
-            <StatsRow title={"Часов в игре"} value={"52"}/>
-            <StatsRow title={"Решено примеров"} value={"38"}/>
-            <StatsRow title={"Решено уравнений"} value={"-97"}/>
+            {Array.prototype.map.call(STATS, function (stat) {
+                return <StatsRow title={stat.title} value={stat.value}/>
+            }, _this)}
         </div>
     )
 }
