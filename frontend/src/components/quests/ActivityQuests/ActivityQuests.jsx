a7e7387fe0b298bcb1b764998b70d92d8de56b1e
ulver <ivansergshum@gmail.com>
2025-03-14 21:28:52 +0300

    adding sample activity quests

--- frontend/src/components/quests/ActivityQuests/ActivityQuests.jsx
+++ frontend/src/components/quests/ActivityQuests/ActivityQuests.jsx
@@ -1,7 +1,27 @@
+import DayReward from "./DayReward/DayReward.jsx";
+import React from "react";
+import "./ActivityQuests.css"
+
+const DAYS = [
+    {reward: 10},
+    {reward: 30},
+    {reward: 50},
+    {reward: 100},
+    {reward: 300},
+    {reward: 500},
+    {reward: 1000},
+];
+
 const ActivityQuests = () => {
+    const _this = this;
     return (
         <div className="ActivityQuests">
-            активность
+            <div className="days">
+                {Array.prototype.map.call(DAYS, function (day, index) {
+                    return <DayReward day={index + 1} reward={day.reward}/>;
+                }, _this)}
+            </div>
+
         </div>
     )
 }
